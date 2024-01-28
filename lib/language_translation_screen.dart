92965ec72ab61f6080d14cee72317060f8b9430e
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2024-01-28 19:22:10 +0530

    Second Commit

--- lib/language_translation_screen.dart
+++ lib/language_translation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 
 class LanguageTranslationPage extends StatefulWidget {
   const LanguageTranslationPage({super.key});
@@ -9,16 +10,45 @@ class LanguageTranslationPage extends StatefulWidget {
 }
 
 class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
-  var language = ["Hindi", "English", "Marathi"];
-  var originLanguage = "From";
-  var destinationLanguage = "To";
-  var output = "";
   TextEditingController languageController = TextEditingController();
+  var output = "";
+
+  void translate(String src, String dest, String input) async {
+    GoogleTranslator translator = new GoogleTranslator();
+    var translation = await translator.translate(input, from: src, to: dest);
+
+    setState(() {
+      output = translation.text.toString();
+    });
+
+    if (src == '--' || dest == '--') {
+      setState(() {
+        output = "Fail to translate";
+      });
+    }
+  }
+
+  String getLanguageCode(String language) {
+    if(language=="English") {
+      return "en";
+    }else if(language=="Hindi"){
+      return "hi";
+    }else if (language == "Arabic") {
+      return "ar";
+    }
+    return "--";
+  }
+
   @override
   Widget build(BuildContext context) {
+    var language = ["Hindi", "English", "Marathi"];
+    var originLanguage = "From";
+    var destinationLanguage = "To";
+
     return Scaffold(
+        backgroundColor: const Color(0xff10223d),
       appBar: AppBar(
-        title: const Text("Labguage Translator"),
+        title: const Text("Language Translator",style: TextStyle(color: Colors.white),),
         centerTitle: true,
         backgroundColor: const Color(0xff10223d),
         elevation: 0,
@@ -34,17 +64,103 @@ class _LanguageTranslationPageState extends State<LanguageTranslationPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   DropdownButton(
-                    items: items,
-                    onChanged: onChanged,
+                    items: language.map((String dropDownStringItem) {
+                      return DropdownMenuItem(
+                        value: dropDownStringItem,
+                        child: Text(dropDownStringItem),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        originLanguage = value!;
+                      });
+                    },
                     focusColor: Colors.white,
                     iconDisabledColor: Colors.white,
                     iconEnabledColor: Colors.white,
-                    hint: Text(originLanguage,style: const TextStyle(color: Colors.white),),
+                    hint: Text(
+                      originLanguage,
+                      style: const TextStyle(color: Colors.white),
+                    ),
                     dropdownColor: Colors.white,
                     icon: const Icon(Icons.keyboard_arrow_down),
-                  )
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  const Icon(
+                    Icons.arrow_right_alt_outlined,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  DropdownButton(
+                    items: language.map((String dropDownStringItem) {
+                      return DropdownMenuItem(
+                        value: dropDownStringItem,
+                        child: Text(dropDownStringItem),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        destinationLanguage = value!;
+                      });
+                    },
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(
+                      destinationLanguage,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    dropdownColor: Colors.white,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                  ),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: TextFormField(
+                  cursorColor: Colors.white,
+                  autofocus: false,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Please Enter your text..',
+                    labelStyle: TextStyle(fontSize: 15, color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                    ),
+                    errorStyle: TextStyle(color: Colors.red, fontSize: 15),
+                  ),
+                  controller: languageController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please Enter Valid Input";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: const Color(0xff2b3c5a)),
+                    onPressed: () {
+                      translate(getLanguageCode(originLanguage), getLanguageCode(destinationLanguage), languageController.text.toString());
+                    },
+                    child: const Text("Translate")),
+              ),
+            const  SizedBox(height: 20,),
+              Text("\n$output",
+              style: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),)
             ],
           ),
         ),
